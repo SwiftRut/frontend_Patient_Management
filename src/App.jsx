c4c4101abb36d3bbf4b924bc52637b6aae470018
Jaydip Makawana <jaydip>
2024-10-03 05:04:07 +0530

    router done

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ function App() {
           <Route path="/registration" element={<Registration />} />
           <Route path="/login" element={<Login />} />
         </Routes>
-      </BrowserRouter> */}
+      </BrowserRouter>
     </>
   );
 }
