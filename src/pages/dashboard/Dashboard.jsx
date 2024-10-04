e38343f973b668247eba7cf089a1d6cca61d2449
Jigar Makawana <jigarmakwana34065@gmail.com>
2024-10-05 03:24:52 +0530

    working on dashboard

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -1,3 +1,148 @@
-export default function Dashboard() {
-  return <div>Dashboard</div>;
+import React from 'react'
+import '../dashboard/dashboard.css'
+import { FaUsers } from "react-icons/fa";
+import { FaUser } from "react-icons/fa";
+import { FaFileAlt } from "react-icons/fa";
+import { FaAddressCard } from "react-icons/fa";
+import { FaBox } from "react-icons/fa";
+import { FaRegStopCircle } from "react-icons/fa";
+
+const Dashboard = () => {
+  return (
+    <>
+      <div className="deshbord-section">
+        <div className="row">
+          <div className="main">
+            <div className="top flex">
+              <div className="Patients-data">
+                <div className="total-data flex">
+                  <div className="total-Patients">
+                    <div className="content">
+                      <div className="logo">
+                        <FaUsers />
+                      </div>
+                      <div className="details">
+                        <p>Total Patients</p>
+                        <span>00</span>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="total-Docters">
+                    <div className="content">
+                      <div className="logo dr-logo">
+                        <FaUser />
+                      </div>
+                      <div className="details">
+                        <p>Total Docters</p>
+                        <span>00</span>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="total-Appointments">
+                    <div className="content">
+                      <div className="logo appo-logo">
+                        <FaFileAlt />
+                      </div>
+                      <div className="details">
+                        <p>Total Appointments</p>
+                        <span>00</span>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="Patients-Statistics">
+                  <div className="patients-content">
+                    <div className="head flex">
+                      <div className="title">
+                        <p>Patients Statistics</p>
+                      </div>
+                      <div className="menu">
+                        <ul className='flex'>
+                          <li> <a href="">Year</a></li>
+                          <li><a href="">Month</a></li>
+                          <li><a href="">Week</a></li>
+                        </ul>
+                      </div>
+                    </div>
+                    <div className="graph">
+
+                    </div>
+                  </div>
+
+                </div>
+              </div>
+              <div className="Billing-data">
+                <div className="content">
+                  <div className="head flex">
+                    <div className="title">
+                      <p>Billing & Payments</p>
+                    </div>
+                    <div className="btn">
+                      <button className='flex'><FaAddressCard />Create Bills</button>
+                    </div>
+                  </div>
+                  <div className="pending-bill">
+                    <div className="bill-status">
+                      <p>Pending Bills : <span>00</span></p>
+                    </div>
+                    <div className="pending-bill-data">
+                      <div className="img">
+                        <img src="../img/FrameBill.png" alt="" />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="bottom flex">
+              <div className="appointments-data">
+                <div className="today-appointments">
+                  <div className="appointments-content">
+                    <div className="head">
+                      <div className="title">
+                        <p>Todays Appointments List</p>
+                      </div>
+                    </div>
+                    <div className="img">
+                      <img src="../img/Frame1.png" alt="" />
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div className="Summary-data">
+                <div className="content">
+                  <div className="head">
+                    <div className="title">
+                      <p>Patients Summary</p>
+                    </div>
+                  </div>
+                  <div className="Summary-status">
+                    <div className="Patients-data flex">
+                      <div className="img">
+                        <img src="../img/Group.png" alt="" />
+                      </div>
+                      <div className="details">
+                        <div className="content">
+                          <ul>
+                            <li className='new'><FaBox />New Patients <span>0</span></li>
+                            <li className='old'><FaBox />Old Patients <span>0</span></li>
+                            <li className='total'><FaBox />Total Patients <span>0</span></li>
+                          </ul>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+          </div>
+        </div>
+      </div>
+    </>
+  )
 }
+
+export default Dashboard
