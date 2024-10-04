cf60084e5eb56cb0b40c88d87e7446b3eca83861
mohitdudhatverselix <mohitdudhat22@gmail.com>
2024-10-04 05:50:28 +0530

    chat and charted added

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,9 @@ import Login from "./pages/Login";
 import Registration from "./pages/Registration";
 import "./component/sidebar.css";
 import AdminPanel from "./pages/AdminPanel";
+import { Chart } from "chart.js";
+import AdminRegistration from "./pages/AdminRegistration";
+import ChatApp from "./pages/Chat";
 
 function App() {
   return (
@@ -13,6 +16,9 @@ function App() {
           <Routes>
             <Route path="/registration" element={<Registration />} />
             <Route path="/login" element={<Login />} />
+            <Route path="/admin-registeration" element={<AdminRegistration />} />
+            <Route path="/charts" element={<Chart />} />
+            <Route path="/chat" element={<ChatApp />} />
             <Route path="/" element={<AdminPanel />}>
               <Route path="doctorManagement" />
               <Route path="profile/*" />
