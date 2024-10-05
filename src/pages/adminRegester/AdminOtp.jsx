2a7da9e1a10c06eb0e1d6323eac5928dcc3c62d4
mayurhapani <hapanimayur@gmail.com>
2024-10-06 02:21:39 +0530

    admin otp change

--- src/pages/adminRegester/AdminOtp.jsx
+++ src/pages/adminRegester/AdminOtp.jsx
@@ -1,8 +1,30 @@
-import React from 'react'
 import "../pages.css";
 import { IoTimeOutline } from "react-icons/io5";
+import { useRef } from "react";
 
 export default function AdminOtp() {
+  const inputRefs = useRef([]);
+
+  const handleInputChange = (index, event) => {
+    const { value } = event.target;
+
+    if (!/^\d*$/.test(value)) {
+      // If the input is not a digit, clear the input
+      event.target.value = "";
+      return;
+    }
+
+    if (value.length === 1 && index < inputRefs.current.length - 1) {
+      inputRefs.current[index + 1].focus();
+    }
+  };
+
+  const handleKeyDown = (index, event) => {
+    if (event.key === "Backspace" && index > 0 && event.target.value === "") {
+      inputRefs.current[index - 1].focus();
+    }
+  };
+
   return (
     <div>
       <div className="admin-otp-section">
@@ -20,48 +42,28 @@ export default function AdminOtp() {
 
                 <div className="admin-otp-form-box">
                   <form className="flex">
-
-                    <div className="input-box">
-                      <input type="number"
-                        placeholder="0"
-                      />
-                    </div>
-
-                    <div className="input-box">
-                      <input type="number"
-                        placeholder="0"
-                      />
-                    </div>
-
-                    <div className="input-box">
-                      <input type="number"
-                        placeholder="0"
-                      />
-                    </div>
-
-                    <div className="input-box">
-                      <input type="number"
-                        placeholder="0"
-                      />
-                    </div>
-
-                    <div className="input-box">
-                      <input type="number"
-                        placeholder="0"
-                      />
-                    </div>
-
-                    <div className="input-box">
-                      <input type="number"
-                        placeholder="0"
-                      />
-                    </div>
+                    {[...Array(6)].map((_, index) => (
+                      <div className="input-box" key={index}>
+                        <input
+                          type="text"
+                          maxLength="1"
+                          pattern="\d*"
+                          inputMode="numeric"
+                          ref={(el) => (inputRefs.current[index] = el)}
+                          onChange={(e) => handleInputChange(index, e)}
+                          onKeyDown={(e) => handleKeyDown(index, e)}
+                          style={{ MozAppearance: "textfield" }}
+                        />
+                      </div>
+                    ))}
 
                     <div className="condition">
-
                       <div className="resend-otp flex">
                         <div className="sec">
-                          <p className='flex'><IoTimeOutline /><span>00:30</span>sec</p>
+                          <p className="flex">
+                            <IoTimeOutline />
+                            <span>00:30</span>sec
+                          </p>
                         </div>
                         <div className="r-otp">
                           <button>Resend Otp</button>
@@ -71,9 +73,7 @@ export default function AdminOtp() {
                       <div className="verify">
                         <button type="submit">Verify</button>
                       </div>
-
                     </div>
-
                   </form>
                 </div>
               </div>
@@ -83,5 +83,5 @@ export default function AdminOtp() {
         </div>
       </div>
     </div>
-  )
+  );
 }
