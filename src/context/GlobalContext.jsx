4c29c25a4310d3988943d269ce5b555d69179dff
mohitdudhat22 <mohitdudhat22@gmail.com>
2024-10-05 07:07:38 +0530

    last commit -m  of the day

--- src/context/GlobalContext.jsx
+++ src/context/GlobalContext.jsx
@@ -1,5 +1,4 @@
 import { createContext, useState, useContext, useEffect } from 'react';
-import axios from 'axios';
 import apiService from '../services/api';
 import PropTypes from 'prop-types';
 const GlobalContext = createContext();
