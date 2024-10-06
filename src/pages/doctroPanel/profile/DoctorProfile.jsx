6269462b002d98a55a21ece05a15af53303c10a9
Jaydip Makawana <jaydip>
2024-10-07 04:49:21 +0530

    working on doctor profile

--- src/pages/doctroPanel/profile/DoctorProfile.jsx
+++ src/pages/doctroPanel/profile/DoctorProfile.jsx
@@ -1,5 +1,33 @@
 import React from "react";
+import { DoctorAside } from "./DoctorAside";
+import DoctorProfileData from "./DoctorProfileData";
+import { Route, Routes } from "react-router-dom";
 
 export default function DoctorProfile() {
-  return <div>DoctorProfile</div>;
+  return <div>
+    <div className="profile-section">
+        <div className="row">
+          <div className="main">
+            <div className="top"></div>
+            <div className="profile-setting">
+              <div className="head">
+                <p>Profile Setting</p>
+              </div>
+              <div className="content flex">
+                <DoctorAside />
+
+                <div className="right">
+                  <Routes>
+                    {/* <Route path="/doctor/profile" element={<DoctorProfileData />} /> */}
+                    {/* <Route path="changePassword" element={<ProfileChangePassword />} /> */}
+                    {/* <Route path="termsCondition" element={<ProfileTermsCondition />} /> */}
+                    {/* <Route path="privacyPolicy" element={<ProfilePrivacyPolicy />} /> */}
+                  </Routes>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+  </div>;
 }
