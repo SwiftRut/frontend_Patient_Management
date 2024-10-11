9554e9346f1b72cf29522c7afc2fd075817be0e3
mayurhapani <hapanimayur@gmail.com>
2024-10-11 11:25:53 +0530

    few deleted file recover

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,25 +1,26 @@
-import { createContext, useState, useContext } from 'react';
-import axios from 'axios';
-import apiService from '../services/api';
-import PropTypes from 'prop-types';
-const AuthContext = createContext();
+import { createContext, useState } from "react";
+import axios from "axios";
+import apiService from "../services/api";
+import PropTypes from "prop-types";
+import { useGlobal } from "../hooks/useGlobal";
+
+export const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
-  const [user, setUser] = useState(JSON.parse(localStorage.getItem('user')) || "");
+  const [user, setUser] = useState(JSON.parse(localStorage.getItem("user")) || "");
   const [loading, setLoading] = useState(true);
-
+  const { getAdminProfile, getDoctorProfile } = useGlobal();
   const PatientLogin = async (userData) => {
-    console.log(userData);
     setLoading(true);
     try {
       const response = await apiService.PatientLogin(userData);
-      localStorage.setItem('token', response.data.token);
-      localStorage.setItem('user', JSON.stringify(response.data.patient));
-      axios.defaults.headers.common['Authorization'] = `Bearer ${response.data.token}`;
+      localStorage.setItem("token", response.data.token);
+      localStorage.setItem("user", JSON.stringify(response.data.patient));
+      axios.defaults.headers.common["Authorization"] = `Bearer ${response.data.token}`;
       setUser(response.data.user);
       return true;
     } catch (error) {
-      console.error('Login error:', error);
+      console.error("Login error:", error);
       throw error;
     } finally {
       setLoading(false);
@@ -27,15 +28,14 @@ export const AuthProvider = ({ children }) => {
   };
 
   const PatientRegister = async (userData) => {
-    console.log(userData);
     setLoading(true);
     try {
       const { data } = await apiService.PatientRegister(userData);
-      localStorage.setItem('token', data.token);
-      axios.defaults.headers.common['Authorization'] = `Bearer ${data.token}`;
+      localStorage.setItem("token", data.token);
+      axios.defaults.headers.common["Authorization"] = `Bearer ${data.token}`;
       setUser(data.user);
     } catch (error) {
-      console.error('Registration error:', error);
+      console.error("Registration error:", error);
       throw error;
     } finally {
       setLoading(false);
@@ -43,17 +43,16 @@ export const AuthProvider = ({ children }) => {
   };
 
   const AdminLogin = async (userData) => {
-    console.log(userData);
     setLoading(true);
     try {
       const response = await apiService.AdminLogin(userData);
-      localStorage.setItem('token', response.data.token);
-      localStorage.setItem('user', JSON.stringify(response.data.patient));
-      axios.defaults.headers.common['Authorization'] = `Bearer ${response.data.token}`;
+      localStorage.setItem("token", response.data.token);
+      localStorage.setItem("user", JSON.stringify(response.data.patient));
+      axios.defaults.headers.common["Authorization"] = `Bearer ${response.data.token}`;
       setUser(response.data.user);
       return true;
     } catch (error) {
-      console.error('Login error:', error);
+      console.error("Login error:", error);
       throw error;
     } finally {
       setLoading(false);
@@ -61,15 +60,14 @@ export const AuthProvider = ({ children }) => {
   };
 
   const AdminRegister = async (userData) => {
-    console.log(userData);
     setLoading(true);
     try {
       const { data } = await apiService.AdminRegister(userData);
-      localStorage.setItem('token', data.token);
-      axios.defaults.headers.common['Authorization'] = `Bearer ${data.token}`;
+      localStorage.setItem("token", data.token);
+      axios.defaults.headers.common["Authorization"] = `Bearer ${data.token}`;
       setUser(data.user);
     } catch (error) {
-      console.error('Registration error:', error);
+      console.error("Registration error:", error);
       throw error;
     } finally {
       setLoading(false);
@@ -77,17 +75,16 @@ export const AuthProvider = ({ children }) => {
   };
 
   const DoctorLogin = async (userData) => {
-    console.log(userData);
     setLoading(true);
     try {
       const response = await apiService.DoctorLogin(userData);
-      localStorage.setItem('token', response.data.token);
-      localStorage.setItem('user', JSON.stringify(response.data.user));
-      axios.defaults.headers.common['Authorization'] = `Bearer ${response.data.token}`;
+      localStorage.setItem("token", response.data.token);
+      localStorage.setItem("user", JSON.stringify(response.data.user));
+      axios.defaults.headers.common["Authorization"] = `Bearer ${response.data.token}`;
       setUser(response.data.user);
       return true;
     } catch (error) {
-      console.error('Login error:', error);
+      console.error("Login error:", error);
       throw error;
     } finally {
       setLoading(false);
@@ -95,44 +92,65 @@ export const AuthProvider = ({ children }) => {
   };
 
   const UniversalLogin = async function (userData) {
-    console.log(userData);
+    console.log("inside UniversalLogin", userData);
     setLoading(true);
     try {
       const response = await apiService.UniversalLogin(userData);
-      localStorage.setItem('token', response.data.token);
-      localStorage.setItem('user', JSON.stringify(response.data.user));
-      axios.defaults.headers.common['Authorization'] = `Bearer ${response.data.token}`;
+      localStorage.setItem("token", response.data.token);
+      localStorage.setItem("user", JSON.stringify(response.data.user));
+      axios.defaults.headers.common["Authorization"] = `Bearer ${response.data.token}`;
       setUser(response.data.user);
+
+      //here we have call the function based on the user role
+      if (response.data.user.role === "doctor") {
+        console.log("doctor profile fetching");
+        await getDoctorProfile(response.data.user.id);
+      } else if (response.data.user.role === "admin") {
+        console.log("admin profile fetching");
+        await getAdminProfile(response.data.user.id);
+      }
       return true;
     } catch (error) {
-      console.error('Login error:', error);
+      console.error("Login error:", error);
       throw error;
     } finally {
       setLoading(false);
     }
-  }
+  };
 
   const logout = async () => {
     try {
       await apiService.UniversalLogout();
-      
-      localStorage.removeItem('token');
-      localStorage.removeItem('user');
-      delete axios.defaults.headers.common['Authorization'];
-      window.location.href = '/login';
+
+      localStorage.removeItem("token");
+      localStorage.removeItem("user");
+      delete axios.defaults.headers.common["Authorization"];
+      window.location.href = "/login";
       setUser(null);
     } catch (error) {
-      console.error('Logout failed:', error);
+      console.error("Logout failed:", error);
     }
   };
 
   return (
-    <AuthContext.Provider value={{ user, loading, PatientLogin, logout, PatientRegister, DoctorLogin, AdminLogin, AdminRegister, UniversalLogin }}>
+    <AuthContext.Provider
+      value={{
+        user,
+        loading,
+        PatientLogin,
+        logout,
+        PatientRegister,
+        DoctorLogin,
+        AdminLogin,
+        AdminRegister,
+        UniversalLogin,
+      }}
+    >
       {children}
     </AuthContext.Provider>
   );
 };
+
 AuthProvider.propTypes = {
   children: PropTypes.node.isRequired,
 };
-export const useAuth = () => useContext(AuthContext);
