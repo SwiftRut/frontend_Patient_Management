5d793be8079010c6822e21d41cb0b93088d76eab
priyanka-dudhat-27 <pdudhat27@gmail.com>
2024-10-16 02:29:41 +0530

    updated model css color

--- src/pages/doctroPanel/AppointmentManagement.jsx
+++ src/pages/doctroPanel/AppointmentManagement.jsx
@@ -124,120 +124,121 @@ export default function AppointmentManagement() {
   });
 
   return (
-    <div className="p-6 bg-white rounded-lg shadow-md m-6">
-      {/* Tabs */}
-      <div className="flex justify-between items-center mb-4">
-        <div className="flex space-x-8 text-sm font-semibold text-gray-500 thead">
-          {[
-            "Today Appointment",
-            "Upcoming Appointment",
-            "Previous Appointment",
-            "Cancel Appointment",
-          ].map((tab) => (
-            <Button
-              key={tab}
-              className={
-                activeTab === tab
-                  ? "!text-[#0EABEB] !border-b-2 !border-[#0EABEB]"
-                  : "text-gray-400"
-              }
-              onClick={() => {
-                setActiveTab(tab);
-              }}
-            >
-              {tab}
-            </Button>
-          ))}
-        </div>
-      </div>
-
-      <div className="menu flex  justify-between items-center mb-3">
-        <h3>{activeTab}</h3>
-        <div className="">
-          {/* Search Bar and Appointment Time Slot */}
-          <div className=" flex items-center space-x-4">
-            {/* Search Field */}
-
-            <TextField
-              className="search outline-none"
-              variant="outlined"
-              placeholder="Search Patient"
-              value={searchTerm}
-              onChange={(e) => setSearchTerm(e.target.value)}
-              InputProps={{
-                startAdornment: (
-                  <InputAdornment position="start">
-                    <Search />
-                  </InputAdornment>
-                ),
-              }}
-            />
-
-            {/* Appointment Time Slot Button */}
-            <div className="time-slot">
+    <div className="bg-[#e4e3e3] p-6 h-full">
+      <div className="p-6 bg-white rounded-lg shadow-md h-full">
+        {/* Tabs */}
+        <div className="flex justify-between items-center mb-4">
+          <div className="flex space-x-8 text-sm font-semibold text-gray-500 thead">
+            {[
+              "Today Appointment",
+              "Upcoming Appointment",
+              "Previous Appointment",
+              "Cancel Appointment",
+            ].map((tab) => (
               <Button
-                variant="contained"
-                color="primary"
-                className="!text-sm time-slot"
-                onClick={() => navigate("/doctor/appointmentTimeSlot")}
+                key={tab}
+                className={
+                  activeTab === tab
+                    ? "!text-[#0EABEB] !border-b-2 !border-[#0EABEB]"
+                    : "text-gray-400"
+                }
+                onClick={() => {
+                  setActiveTab(tab);
+                }}
               >
-                Appointment Time Slot
+                {tab}
               </Button>
+            ))}
+          </div>
+        </div>
+
+        <div className="menu flex justify-between items-center mb-3">
+          <h3>{activeTab}</h3>
+          <div className="">
+            {/* Search Bar and Appointment Time Slot */}
+            <div className="flex items-center space-x-4">
+              {/* Search Field */}
+              <TextField
+                className="search outline-none"
+                variant="outlined"
+                placeholder="Search Patient"
+                value={searchTerm}
+                onChange={(e) => setSearchTerm(e.target.value)}
+                InputProps={{
+                  startAdornment: (
+                    <InputAdornment position="start">
+                      <Search />
+                    </InputAdornment>
+                  ),
+                }}
+              />
+
+              {/* Appointment Time Slot Button */}
+              <div className="time-slot">
+                <Button
+                  variant="contained"
+                  color="primary"
+                  className="!text-sm time-slot"
+                  onClick={() => navigate("/doctor/appointmentTimeSlot")}
+                >
+                  Appointment Time Slot
+                </Button>
+              </div>
             </div>
           </div>
         </div>
-      </div>
 
-      {/* Table of Appointments */}
-      <div className="max-h-[600px] overflow-y-auto t-data">
-        <table className="min-w-full table-auto">
-          <thead className="sticky top-0 bg-gray-100 z-10">
-            <tr>
-              <th className="p-3 text-left text-sm font-semibold">Patient Name</th>
-              <th className="p-3 text-left text-sm font-semibold">Disease Name</th>
-              <th className="p-3 text-left text-sm font-semibold">Patient Issue</th>
-              <th className="p-3 text-left text-sm font-semibold">Appointment Date</th>
-              <th className="p-3 text-left text-sm font-semibold">Appointment Time</th>
-              <th className="p-3 text-left text-sm font-semibold">Appointment Type</th>
-              <th className="p-3 text-left text-sm font-semibold">Action</th>
-            </tr>
-          </thead>
-          <tbody>
-            {filteredAppointments.map((appointment, index) => (
-              <tr key={index} className="border-t">
-                <td className="p-3">{appointment.patientName}</td>
-                <td className="p-3">{appointment.diseaseName}</td>
-                <td className="p-3">{appointment.patientIssue}</td>
-                <td className="p-3">{appointment.appointmentDate}</td>
-                <td className="p-3 text-blue-600">
-                  <span className="a-time">{appointment.appointmentTime}</span>
-                </td>
-                <td className="p-3">
-                  <h3
-                    className={`px-3 py-1 text-sm font-medium rounded-full ${appointment.appointmentType === "Online" ? "bg-yellow-100 text-yellow-600" : "bg-blue-100 text-blue-600"}`}
-                  >
-                    {appointment.appointmentType}
-                  </h3>
-                </td>
-                <td className="p-3 btn">
-                  <IconButton color="primary" onClick={() => setOpenCustomDateModal(true)}>
-                    <CalendarToday style={{ color: "#E11D29" }} />
-                  </IconButton>
-
-                  <IconButton color="secondary" onClick={() => setOpenCancelAppointmentModal(true)}>
-                    <CalendarToday style={{ color: "#5678E9" }} />
-                  </IconButton>
-                </td>
+        {/* Table of Appointments */}
+        <div className="flex-grow overflow-y-auto t-data">
+          <table className="min-w-full table-auto">
+            <thead className="sticky top-0 bg-gray-100 z-10">
+              <tr>
+                <th className="p-3 text-left text-sm font-semibold">Patient Name</th>
+                <th className="p-3 text-left text-sm font-semibold">Disease Name</th>
+                <th className="p-3 text-left text-sm font-semibold">Patient Issue</th>
+                <th className="p-3 text-left text-sm font-semibold">Appointment Date</th>
+                <th className="p-3 text-left text-sm font-semibold">Appointment Time</th>
+                <th className="p-3 text-left text-sm font-semibold">Appointment Type</th>
+                <th className="p-3 text-left text-sm font-semibold">Action</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
+            </thead>
+            <tbody>
+              {filteredAppointments.map((appointment, index) => (
+                <tr key={index} className="border-t">
+                  <td className="p-3">{appointment.patientName}</td>
+                  <td className="p-3">{appointment.diseaseName}</td>
+                  <td className="p-3">{appointment.patientIssue}</td>
+                  <td className="p-3">{appointment.appointmentDate}</td>
+                  <td className="p-3 text-blue-600">
+                    <span className="a-time">{appointment.appointmentTime}</span>
+                  </td>
+                  <td className="p-3">
+                    <h3
+                      className={`px-3 py-1 text-sm font-medium rounded-full ${appointment.appointmentType === "Online" ? "bg-yellow-100 text-yellow-600" : "bg-blue-100 text-blue-600"}`}
+                    >
+                      {appointment.appointmentType}
+                    </h3>
+                  </td>
+                  <td className="p-3 btn">
+                    <IconButton color="primary" onClick={() => setOpenCustomDateModal(true)}>
+                      <CalendarToday style={{ color: "#5678E9" }} />
+                    </IconButton>
+
+                    <IconButton color="secondary" onClick={() => setOpenCancelAppointmentModal(true)}>
+                      <CalendarToday style={{ color: "#E11D29" }} />
+                    </IconButton>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+        <CustomDateModal open={openCustomDateModal} onClose={() => setOpenCustomDateModal(false)} />
+        <CancelAppointmentModal
+          open={openCancelAppointmentModal}
+          onClose={() => setOpenCancelAppointmentModal(false)}
+        />
       </div>
-      <CustomDateModal open={openCustomDateModal} onClose={() => setOpenCustomDateModal(false)} />
-      <CancelAppointmentModal
-        open={openCancelAppointmentModal}
-        onClose={() => setOpenCancelAppointmentModal(false)}
-      />
     </div>
   );
 }
