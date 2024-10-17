c0901d58d6eef76142442f18da7360d496bb3b86
jigarmakwana <jigarmakwana34065@gmail.com>
2024-10-18 03:12:47 +0530

    patient panel / p-health record / medical history modified

--- src/pages/patientPanel/profile/Allappoiment.jsx
+++ src/pages/patientPanel/profile/Allappoiment.jsx
@@ -50,9 +50,134 @@ const AllAppointment = () => {
       patientIssue: "Back pain",
       diseaseName: "Spondylitis",
     },
+    {
+      doctorName: "Dr. Smith",
+      hospitalName: "City Hospital",
+      appointmentType: "Online",
+      appointmentDate: "2024-10-01",
+      appointmentTime: "10:00 AM",
+      patientIssue: "Fever and headache",
+      diseaseName: "Flu",
+    },
+    {
+      doctorName: "Dr. Johnson",
+      hospitalName: "Green Valley Clinic",
+      appointmentType: "Offline",
+      appointmentDate: "2024-09-28",
+      appointmentTime: "2:00 PM",
+      patientIssue: "Chest pain",
+      diseaseName: "Angina",
+    },
+    {
+      doctorName: "Dr. Lee",
+      hospitalName: "Health Center",
+      appointmentType: "Online",
+      appointmentDate: "2024-10-03",
+      appointmentTime: "1:00 PM",
+      patientIssue: "Skin rash",
+      diseaseName: "Allergy",
+    },
+    {
+      doctorName: "Dr. Brown",
+      hospitalName: "City Hospital",
+      appointmentType: "Offline",
+      appointmentDate: "2024-09-30",
+      appointmentTime: "9:00 AM",
+      patientIssue: "Knee pain",
+      diseaseName: "Arthritis",
+    },
+    {
+      doctorName: "Dr. White",
+      hospitalName: "Health Plus Clinic",
+      appointmentType: "Online",
+      appointmentDate: "2024-10-02",
+      appointmentTime: "3:00 PM",
+      patientIssue: "Back pain",
+      diseaseName: "Spondylitis",
+    },
+    {
+      doctorName: "Dr. Smith",
+      hospitalName: "City Hospital",
+      appointmentType: "Online",
+      appointmentDate: "2024-10-01",
+      appointmentTime: "10:00 AM",
+      patientIssue: "Fever and headache",
+      diseaseName: "Flu",
+    },
+    {
+      doctorName: "Dr. Johnson",
+      hospitalName: "Green Valley Clinic",
+      appointmentType: "Offline",
+      appointmentDate: "2024-09-28",
+      appointmentTime: "2:00 PM",
+      patientIssue: "Chest pain",
+      diseaseName: "Angina",
+    },
+    {
+      doctorName: "Dr. Lee",
+      hospitalName: "Health Center",
+      appointmentType: "Online",
+      appointmentDate: "2024-10-03",
+      appointmentTime: "1:00 PM",
+      patientIssue: "Skin rash",
+      diseaseName: "Allergy",
+    },
+    {
+      doctorName: "Dr. Brown",
+      hospitalName: "City Hospital",
+      appointmentType: "Offline",
+      appointmentDate: "2024-09-30",
+      appointmentTime: "9:00 AM",
+      patientIssue: "Knee pain",
+      diseaseName: "Arthritis",
+    },
+    {
+      doctorName: "Dr. White",
+      hospitalName: "Health Plus Clinic",
+      appointmentType: "Online",
+      appointmentDate: "2024-10-02",
+      appointmentTime: "3:00 PM",
+      patientIssue: "Back pain",
+      diseaseName: "Spondylitis",
+    },
   ];
 
   const description = [
+    {
+      descriptiondate: "2 Jan, 2024",
+      lorem1:
+        "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Temporibus, voluptatibus hic. Repudiandae vel eaque mollitia molestiae at fugiat sed nesciunt! Maxime,",
+      lorem2:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium tempora velit error nobis cum.",
+    },
+    {
+      descriptiondate: "3 Jan, 2024",
+      lorem1:
+        "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Temporibus, voluptatibus hic. Repudiandae vel eaque mollitia molestiae at fugiat sed nesciunt! Maxime,",
+      lorem2:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium tempora velit error nobis cum.",
+    },
+    {
+      descriptiondate: "4 Jan, 2024",
+      lorem1:
+        "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Temporibus, voluptatibus hic. Repudiandae vel eaque mollitia molestiae at fugiat sed nesciunt! Maxime,",
+      lorem2:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium tempora velit error nobis cum.",
+    },
+    {
+      descriptiondate: "5 Jan, 2024",
+      lorem1:
+        "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Temporibus, voluptatibus hic. Repudiandae vel eaque mollitia molestiae at fugiat sed nesciunt! Maxime,",
+      lorem2:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium tempora velit error nobis cum.",
+    },
+    {
+      descriptiondate: "6 Jan, 2024",
+      lorem1:
+        "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Temporibus, voluptatibus hic. Repudiandae vel eaque mollitia molestiae at fugiat sed nesciunt! Maxime,",
+      lorem2:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium tempora velit error nobis cum.",
+    },
     {
       descriptiondate: "2 Jan, 2024",
       lorem1:
@@ -182,7 +307,7 @@ const AllAppointment = () => {
               </div>
             )}
             {activeTab === "descriptions" && (
-              <div className="overflow-y-auto" style={{ height: "550px" }}>
+              <div className="overflow-y-auto" style={{ height: "720px" }}>
                 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
                   {description.map((val, index) => (
                     <div
@@ -190,15 +315,15 @@ const AllAppointment = () => {
                       className="w-full rounded-lg bg-white border border-gray-200 shadow-md h-70"
                     >
                       <div className="flex justify-between items-center py-2 bg-gray-100 px-3">
-                        <h6 className="text-md font-semibold">Description</h6>
-                        <h6>{val.descriptiondate}</h6>
+                        <h6 className="text-[18px] text-[#030229] font-semibold">Description</h6>
+                        <h6 className="text-[18px] text-[#030229] font-semibold">{val.descriptiondate}</h6>
                       </div>
-                      <div className="px-3">
-                        <p>
+                      <div className="p-3">
+                        <p className="text-start">
                           <i className="fa-solid fa-circle pe-2" style={{ fontSize: "6px" }}></i>
                           {val.lorem1}
                         </p>
-                        <p>
+                        <p className="text-start">
                           <i className="fa-solid fa-circle pe-2" style={{ fontSize: "6px" }}></i>
                           {val.lorem2}
                         </p>
