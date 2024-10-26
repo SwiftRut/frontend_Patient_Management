f9386fa49b9857dde707d8c497e28cbb8780f494
mohitdudhat22 <mohitdudhat22@gmail.com>
2024-10-27 02:13:19 +0530

    chagnes

--- src/imports/index.js
+++ src/imports/index.js
@@ -1,50 +1,48 @@
-import { lazyLoader } from "../utils/lazyLoader";
-
 // Admin Components
-export const Login = lazyLoader("pages/Login.jsx");
-export const AdminRegistration = lazyLoader("pages/adminRegister/AdminRegistration.jsx");
-export const AdminMobile = lazyLoader("pages/adminRegister/AdminMobile.jsx");
-export const AdminOtp = lazyLoader("pages/adminRegister/AdminOtp.jsx");
-export const AdminChangePassword = lazyLoader("pages/adminRegister/AdminChangePassword.jsx");
-export const AdminPanel = lazyLoader("pages/AdminPanel.jsx");
+export { default as Login } from "../pages/Login.jsx";
+export { default as AdminRegistration } from "../pages/adminRegister/AdminRegistration.jsx";
+export { default as AdminMobile } from "../pages/adminRegister/AdminMobile.jsx";
+export { default as AdminOtp } from "../pages/adminRegister/AdminOtp.jsx";
+export { default as AdminChangePassword } from "../pages/adminRegister/AdminChangePassword.jsx";
+export { default as AdminPanel } from "../pages/AdminPanel.jsx";
 
 // Invoice Components
-export const Invoice = lazyLoader("pages/invoice/Invoice.jsx");
-export const CreateBill = lazyLoader("pages/invoice/CreateBill.jsx");
-export const EditBill = lazyLoader("pages/invoice/EditBill.jsx");
+export { default as Invoice } from "../pages/invoice/Invoice.jsx";
+export { default as CreateBill } from "../pages/invoice/CreateBill.jsx";
+export { default as EditBill } from "../pages/invoice/EditBill.jsx";
 
 // Doctor Components
-export const DoctorPanel = lazyLoader("pages/doctroPanel/DoctorPanel.jsx");
-export const DoctorProfile = lazyLoader("pages/doctroPanel/profile/DoctorProfile.jsx");
+export { default as DoctorPanel } from "../pages/doctroPanel/DoctorPanel.jsx";
+export { default as DoctorProfile } from "../pages/doctroPanel/profile/DoctorProfile.jsx";
 
 // Patient Components
-export const PatientRegistration = lazyLoader("pages/PatientRegistration.jsx");
-export const PatientPanel = lazyLoader("pages/patientPanel/PatientPanel.jsx");
-export const PersonalHealthRecord = lazyLoader("pages/patientPanel/profile/PersonalHealthRecord.jsx");
-export const PatientDetails = lazyLoader("pages/patientManagement/PatientDetails.jsx");
-export const PatientProfile = lazyLoader("pages/patientPanel/profile/PatientProfile.jsx");
-export const Prescriptions = lazyLoader("pages/patientPanel/profile/Prescriptions.jsx");
-export const TestReport = lazyLoader("pages/patientPanel/profile/TestReport.jsx");
-export const MedicalHistory = lazyLoader("pages/patientPanel/profile/MedicalHistory.jsx");
-export const AllAppointment = lazyLoader("pages/patientPanel/profile/Allappoiment.jsx");
-export const Appointment = lazyLoader("pages/patientPanel/Appointment.jsx");
-export const AppointmentBooking = lazyLoader("pages/patientPanel/AppointmentBooking.jsx");
-export const ChatScreen1 = lazyLoader("pages/patientPanel/ChatScreen1.jsx");
+export { default as PatientRegistration } from "../pages/PatientRegistration.jsx";
+export { default as PatientPanel } from "../pages/patientPanel/PatientPanel.jsx";
+export { default as PersonalHealthRecord } from "../pages/patientPanel/profile/PersonalHealthRecord.jsx";
+export { default as PatientDetails } from "../pages/patientManagement/PatientDetails.jsx";
+export { default as PatientProfile } from "../pages/patientPanel/profile/PatientProfile.jsx";
+export { default as Prescriptions } from "../pages/patientPanel/profile/Prescriptions.jsx";
+export { default as TestReport } from "../pages/patientPanel/profile/TestReport.jsx";
+export { default as MedicalHistory } from "../pages/patientPanel/profile/MedicalHistory.jsx";
+export { default as AllAppointment } from "../pages/patientPanel/profile/Allappoiment.jsx";
+export { default as Appointment } from "../pages/patientPanel/Appointment.jsx";
+export { default as AppointmentBooking } from "../pages/patientPanel/AppointmentBooking.jsx";
+export { default as ChatScreen1 } from "../pages/patientPanel/ChatScreen1.jsx";
 
 // Extra Components
-export const Scheduler = lazyLoader("component/Schedular.jsx");
-export const Bill = lazyLoader("component/Bill.jsx");
-export const Onsite = lazyLoader("pages/doctorManagement/Onsite.jsx");
-export const CashPayment = lazyLoader("pages/billPayment/CashPayment.jsx");
-export const Delete = lazyLoader("pages/doctorManagement/Delete.jsx");
-export const Bill2 = lazyLoader("pages/invoice/Bill2.jsx");
-export const EditDesignInvoice = lazyLoader("pages/billPayment/EditDesignInvoice.jsx");
-export const Bill3 = lazyLoader("pages/invoice/Bill3.jsx");
+export { default as Scheduler } from "../component/Schedular.jsx";
+export { default as Bill } from "../pages/invoice/Bill.jsx";
+export { default as Onsite } from "../pages/doctorManagement/Onsite.jsx";
+export { default as CashPayment } from "../pages/billPayment/CashPayment.jsx";
+export { default as Delete } from "../pages/doctorManagement/Delete.jsx";
+export { default as Bill2 } from "../pages/invoice/Bill2.jsx";
+export { default as EditDesignInvoice } from "../pages/billPayment/EditDesignInvoice.jsx";
+export { default as Bill3 } from "../pages/invoice/Bill3.jsx";
 
 // Charts, Chat, and Video Call
-export const Chart = lazyLoader("pages/Chart.jsx");
-export const Chat = lazyLoader("pages/Chat.jsx");
-export const VideoCall = lazyLoader("VideoCall.jsx");
+export { default as Chart } from "../pages/Chart.jsx";
+export { default as Chat } from "../pages/Chat.jsx";
+export { default as VideoCall } from "../VideoCall.jsx";
 
 // Common Components (not lazy-loaded)
 export { default as Loading } from "../component/common/Loading.jsx";
