79848580d8c67afb8bd37e101eabe16dc3de045d
mohitdudhat22 <mohitdudhat22@gmail.com>
2024-11-04 18:41:14 +0530

    chaneges

--- vite.config.js
+++ vite.config.js
@@ -16,16 +16,29 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     sourcemap: true,
+    commonjsOptions: {
+      include: [/@zegocloud\/zego-uikit-prebuilt/, /node_modules/],
+    },
     rollupOptions: {
       output: {
-        manualChunks: {
-          'react-vendor': ['react', 'react-dom', 'react-router-dom'],
-          'icons': ['react-icons/md', 'react-icons/fa', 'react-icons/ai'],
-        },
-      },
-    },
+        manualChunks(id) {
+          if (id.includes('@zegocloud/zego-uikit-prebuilt')) {
+            return 'zego';
+          }
+          if (id.includes('react') || id.includes('react-dom') || id.includes('react-router-dom')) {
+            return 'react-vendor';
+          }
+          if (id.includes('react-icons/md') || id.includes('react-icons/fa') || id.includes('react-icons/ai')) {
+            return 'icons';
+          }
+        }
+      }
+    }
   },
   optimizeDeps: {
     include: ['react-icons/md', 'use-isomorphic-layout-effect'],
-  },
+    esbuildOptions: {
+      target: 'es2020'
+    }
+  }
 })
