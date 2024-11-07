e241b0e9ee1873491a6f6cc75110d764407957a8
mohitdudhat22 <mohitdudhat22@gmail.com>
2024-11-07 22:49:12 +0530

    lgoin form addedc

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,18 +2,46 @@ import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../hooks/useAuth";
 import { FaEye, FaEyeSlash } from "react-icons/fa";
+import { toast } from "react-hot-toast";
+import { useForm } from "react-hook-form";
 import "./pages.css";
-import {toast} from "react-hot-toast";
+
 const Login = () => {
   const navigate = useNavigate();
   const { UniversalLogin } = useAuth();
-  const [formData, setFormData] = useState({
-    identifier: "",
-    password: "",
-    remember: "",
-  });
-  const [error, setError] = useState("");
   const [showPassword, setShowPassword] = useState(false);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = async (data) => {
+    try {
+      const role = await UniversalLogin(data);
+      if (role) {
+        switch (role) {
+          case "admin":
+            toast.success("Admin login successful");
+            console.log("Navigating to / for admin");
+            window.location.href = "/";
+            break;
+          case "patient":
+            toast.success("Patient login successful");
+            navigate('/patient', { replace: true });
+            break;
+          case "doctor":
+            toast.success("Doctor login successful");
+            navigate('/doctor', { replace: true });
+            break;
+        }
+      }
+    } catch (err) {
+      toast.error("Login failed. Please check your credentials.");
+      console.error(err);
+    }
+  };
 
   useEffect(() => {
     const slider = document.querySelector(".slider");
@@ -38,155 +66,106 @@ const Login = () => {
     }
   }, []);
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData((prevState) => ({
-      ...prevState,
-      [name]: value,
-    }));
-  };
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    try {
-      const role = await UniversalLogin(formData);
-      console.log(role);
-      if (role) {
-        if (role === "admin") {
-          toast.success("admin login successfully")
-          window.location.href = "/";
-        }
-        else if (role === "patient") {
-          toast.success("patient login successfully")
-          window.location.href = "/patient";
-
-        } else if (role === "doctor") {
-          toast.success("doctor login successfully")
-          console.log("navigating in doctor")
-          window.location.href = "/doctor";
-        }
-      }
-    } catch (err) {
-      toast.error("Login failed. Please check your credentials.");
-      console.error(err);
-      setError("Login failed. Please check your credentials.");
-    }
-  };
-
   return (
-    <>
-      <div className="login-section">
-        <div className="row">
-          <div className="main flex">
-            <div className="form ">
-              <div className="content">
-                <div className="head">
-                  <p>Login</p>
-                </div>
-                {error && <p className="error-message">{error}</p>}
-                <div className="form-box">
-                  <form onSubmit={handleSubmit} className="flex">
-                    <div className="input-box">
-                      <div className="label">
-                        Email or Phone <span>*</span>
-                      </div>
+    <div className="login-section">
+      <div className="row">
+        <div className="main flex">
+          <div className="form">
+            <div className="content">
+              <div className="head">
+                <p>Login</p>
+              </div>
+              <div className="form-box">
+                <form onSubmit={handleSubmit(onSubmit)} className="flex">
+                  <div className="input-box">
+                    <div className="label">
+                      Email or Phone <span>*</span>
+                    </div>
+                    <input
+                      type="text"
+                      placeholder="Enter Email or Phone Number"
+                      {...register("identifier", { required: "This field is required" })}
+                    />
+                    {errors.identifier && (
+                      <p className="error-message">{errors.identifier.message}</p>
+                    )}
+                  </div>
+
+                  <div className="input-box">
+                    <div className="label">
+                      Password <span>*</span>
+                    </div>
+                    <div className="password-input-container">
                       <input
-                        type="text"
-                        name="identifier"
-                        value={formData.identifier}
-                        onChange={handleChange}
-                        placeholder="Enter Email or Phone Number"
+                        type={showPassword ? "text" : "password"}
+                        placeholder="Enter Password"
+                        {...register("password", { required: "Password is required" })}
                       />
-                    </div>
-
-                    <div className="input-box">
-                      <div className="label">
-                        Password <span>*</span>
+                      <div className="eye" onClick={() => setShowPassword(!showPassword)}>
+                        {showPassword ? <FaEye size={20} /> : <FaEyeSlash size={20} />}
                       </div>
-                      <div className="password-input-container">
+                    </div>
+                    {errors.password && (
+                      <p className="error-message">{errors.password.message}</p>
+                    )}
+                  </div>
+
+                  <div className="condition">
+                    <div className="remember-forgot flex">
+                      <div className="remember flex">
                         <input
-                          type={showPassword ? "text" : "password"}
-                          name="password"
-                          value={formData.password}
-                          onChange={handleChange}
-                          placeholder="Enter Password"
+                          type="checkbox"
+                          {...register("remember")}
                         />
-                        <div className="eye" onClick={() => setShowPassword(!showPassword)}>
-                          {showPassword ? <FaEye size={20} /> : <FaEyeSlash size={20} />}
-                        </div>
+                        <p>Remember me</p>
+                      </div>
+                      <div className="forgot" onClick={() => navigate("/AdminMobile")}>
+                        <span style={{ cursor: "pointer" }}>Forgot password?</span>
                       </div>
                     </div>
 
-                    <div className="condition">
-                      <div className="remember-forgot flex">
-                        <div className="remember flex">
-                          <input
-                            type="checkbox"
-                            name="remember"
-                            checked={formData.remember === "true"}
-                            onChange={(e) =>
-                              setFormData({
-                                ...formData,
-                                remember: e.target.checked ? "true" : "false",
-                              })
-                            }
-                          />
-                          <p>Remember me</p>
-                        </div>
-                        <div className="forgot" onClick={() => navigate("/AdminMobile")}>
-                          <span
-                            style={{
-                              cursor: "pointer",
-                            }}
-                          >
-                            Forgot password?
-                          </span>
-                        </div>
-                      </div>
-
-                      <div className="login-btn">
-                        <button type="submit">Login</button>
-                      </div>
-                      <div
-                        className="registration-btn"
-                        onClick={() => navigate("/patientRegistration")}
-                      >
-                        <p style={{ cursor: "pointer" }}>Don’t have an account? Register</p>
-                      </div>
-                       <div
-                        className="registration-btn"
-                        onClick={() => navigate("/adminRegistration")}
-                      >
-                        <p style={{ cursor: "pointer" }}>Don’t have an account? Register (As Admin)</p>
-                      </div> 
+                    <div className="login-btn">
+                      <button type="submit">Login</button>
                     </div>
-                  </form>
-                </div>
+                    <div
+                      className="registration-btn"
+                      onClick={() => navigate("/patientRegistration")}
+                    >
+                      <p style={{ cursor: "pointer" }}>Don’t have an account? Register</p>
+                    </div>
+                    <div
+                      className="registration-btn"
+                      onClick={() => navigate("/adminRegistration")}
+                    >
+                      <p style={{ cursor: "pointer" }}>Don’t have an account? Register (As Admin)</p>
+                    </div>
+                  </div>
+                </form>
               </div>
             </div>
-            <div className="img-box">
-              <div className="slider">
-                <img src="/img/register.png" alt="Image 1" />
-                <img src="/img/register2.png" alt="Image 2" />
-                <div className="dots">
-                  <span className="dot active"></span>
-                  <span className="dot"></span>
-                </div>
+          </div>
+          <div className="img-box">
+            <div className="slider">
+              <img src="/img/register.png" alt="Image 1" />
+              <img src="/img/register2.png" alt="Image 2" />
+              <div className="dots">
+                <span className="dot active"></span>
+                <span className="dot"></span>
               </div>
-              <div className="vector-1">
-                <img src="/img/Vector-1.png" width="100%" />
-              </div>
-              <div className="vector-2">
-                <img src="/img/Vector-2.png" width="100%" />
-              </div>
-              <div className="vector-dot">
-                <img src="/img/Vector-dot.png" width="100%" />
-              </div>
+            </div>
+            <div className="vector-1">
+              <img src="/img/Vector-1.png" width="100%" />
+            </div>
+            <div className="vector-2">
+              <img src="/img/Vector-2.png" width="100%" />
+            </div>
+            <div className="vector-dot">
+              <img src="/img/Vector-dot.png" width="100%" />
             </div>
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
