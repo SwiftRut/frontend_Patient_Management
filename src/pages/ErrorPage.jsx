1d00f5f68979c10356c196c29c5dcdbfc3fe7862
priyanka-dudhat-27 <pdudhat27@gmail.com>
2024-11-16 00:10:56 +0530

    errorpage redirect resolved

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,24 +1,51 @@
 import React from "react";
-
+import { useNavigate } from "react-router-dom";
 import "./ErrorPage.css";
+import { useAuth } from "../hooks/useAuth";
 
 export const ErrorPage = () => {
+  const { user } = useAuth();
+  const navigate = useNavigate();
+
+  // Helper function for navigation based on role
+  const navigateBasedOnRole = (role) => {
+    switch (role) {
+      case "admin":
+        window.location.href = "/";
+        break;
+      case "doctor":
+        window.location.href = "/doctor";
+        break;
+      case "patient":
+        window.location.href = "/patient";
+        break;
+      default:
+        window.location.href = "/login";
+    }
+  };
+  
+  // Manual redirection on button click
+  const handleGoHome = () => {
+    if (user) {
+      navigateBasedOnRole(user.role);
+    } else {
+      navigate("/login");
+    }
+  };
+
   return (
-    <section class="page_404">
-      <div class="">
-        <div class="row">
-          <div class="col-sm-12">
-            <div class="col-sm-10 col-sm-offset-1  text-center">
-              <div class="four_zero_four_bg"></div>
-
-              <div class="contant_box_404">
-                <h3 class="h2">Look like you're lost</h3>
-
-                <p>the page you are looking for not avaible!</p>
-
-                <a href="" class="link_404">
+    <section className="page_404">
+      <div className="">
+        <div className="row">
+          <div className="col-sm-12">
+            <div className="col-sm-10 col-sm-offset-1 text-center">
+              <div className="four_zero_four_bg"></div>
+              <div className="contant_box_404">
+                <h3 className="h2">Looks like you're lost</h3>
+                <p>The page you are looking for is not available!</p>
+                <button className="link_404" onClick={handleGoHome}>
                   Go to Home
-                </a>
+                </button>
               </div>
             </div>
           </div>
