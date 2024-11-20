43bae5012caca77d2c781b09b974bf0e0b708288
jigarmakwana <jigarmakwana34065@gmail.com>
2024-11-21 00:56:32 +0530

    invoice css modified

--- src/pages/invoice/EditBill.jsx
+++ src/pages/invoice/EditBill.jsx
@@ -127,7 +127,7 @@ const EditBill = () => {
 
   return (
     <div>
-      <div className="editBill-section">
+      <div className="bill-insurance-section">
         <div className="row">
           <div className="main">
             <div className="title">
